e18b806b52e08a88984dcde792b0b942fbcf18b6
mathema123 <garam1732@snu.ac.kr>
2022-11-27 01:12:24 +0900

    allowed host

--- seminar4/settings.py
+++ seminar4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@^%cy=e9r^mb8s=2&#h&-_eeb^oo@isq20tn6drp-jg_=t0hv*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
